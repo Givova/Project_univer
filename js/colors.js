eec08f6f85ec4201a80ac6ab7139d542e6239574
Kukushka <gusarov.ml@mail.ru>
2024-12-19 17:23:49 +0300

    Kvest add picture

--- js/colors.js
+++ js/colors.js
@@ -1,17 +1,37 @@
-// var colors = ['rgba(204, 204, 255, 1)', 'rgba(255, 204, 255, 1)', 'rgba(204, 153, 255, 1)', 'rgba(102, 51, 255, 1)', 'rgba(204, 51, 255, 1)', 'rgba(255, 255, 255, 1)'];
-// var random_color = colors[Math.random() * colors.length ^ 0];
-// document.getElementById('block_kvests').style.backgroundColor = random_color;
-// // document.getElementById('block_kvests').style.backgroundColor = '#C62A17;'
+
+
+
+// function changeBackgroundColor() {
+//     const colors = ['#E38D25', '#C62A17', '#2F2255', '#55224A']; // Массив цветов
+//     const blocks = document.querySelectorAll('.block_kvests'); // Получение всех элементов
+
+//     blocks.forEach(block => {
+//         const randomIndex = Math.floor(Math.random() * colors.length); // Случайный индекс
+//         const selectedColor = colors[randomIndex]; // Выбор цвета
+//         block.style.backgroundColor = selectedColor; // Установка цвета фона
+//     });
+// }
+
+// // Вызываем функцию при загрузке страницы
+// window.onload = changeBackgroundColor;
 
 function changeBackgroundColor() {
-    const colors = ['#E38D25', '#C62A17', '#2F2255', '#55224A' ]; // Массив цветов 'rgba(85, 34, 74, 1);', 'rgba(47, 34, 85, 1);', 'rgba(198, 42, 23, 1);', 'rgba(227, 141, 37, 1);'
-    const randomIndex = Math.floor(Math.random() * colors.length); // Случайный индекс
-    const selectedColor = colors[randomIndex]; // Выбор цвета
-    const block = document.querySelector('.block_kvests'); // Получение элемента
-    block.style.backgroundColor = selectedColor; // Установка цвета фона
+    const colors = ['#E38D25', '#C62A17', '#2F2255', '#55224A']; // Массив цветов
+    const blocks = document.querySelectorAll('.block_kvests'); // Получение всех элементов
+    let previousColor = null; // Переменная для хранения предыдущего цвета
+
+    blocks.forEach(block => {
+        let selectedColor;
+
+        do {
+            const randomIndex = Math.floor(Math.random() * colors.length); // Случайный индекс
+            selectedColor = colors[randomIndex]; // Выбор цвета
+        } while (selectedColor === previousColor); // Проверка, совпадает ли с предыдущим цветом
+
+        block.style.backgroundColor = selectedColor; // Установка цвета фона
+        previousColor = selectedColor; // Обновляем предыдущий цвет
+    });
 }
 
 // Вызываем функцию при загрузке страницы
 window.onload = changeBackgroundColor;
-
-  
