6877ccf6195098e2263684739de8877c8f17c629
Kukushka <gusarov.ml@mail.ru>
2024-12-19 22:40:00 +0300

    final 100%

--- js/colors.js
+++ js/colors.js
@@ -1,24 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('myButton');
-    const message = document.getElementById('message');
-    const userInput = document.getElementById('userInput');
-    const fam = "аверченков";
-
-
-button.addEventListener('click', function() {
-// Получаем значение из поля ввода
-const inputValue = userInput.value;
-if(inputValue.toLowerCase() == fam){
-             message.textContent = 'Это правильный ответ! Квест оказался слишком лёгким?))';
-         }
-         else{
-             message.textContent = 'Увы! Нужно большу ходить на пары(';
-         }
-});
-});
-
-
-
 
 function changeBackgroundColor() {
     const colors = ['#E38D25', '#C62A17', '#2F2255', '#55224A']; // Массив цветов
@@ -41,27 +20,3 @@ function changeBackgroundColor() {
 // Вызываем функцию при загрузке страницы
 window.onload = changeBackgroundColor;
 // .............................................................................................................................
-
-
-
-
-
-
-
-
-// document.getElementById('inputForm').addEventListener('submit', function(event) {
-//     event.preventDefault(); // Предотвращаем стандартное поведение формы
-
-//     const userInput = document.getElementById('userInput').value;
-//     const correctString = "правильный ответ"; // Заданная строка
-//     let result = 0; // Переменная для хранения результата
-
-//     // Сравнение введенного значения с заданной строкой
-//     if (userInput === correctString) {
-//         result = 1; // Если совпадает
-//     }
-
-//     // Перенаправление на новую страницу с результатом
-//     window.location.href = `result.html?result=${result}`;
-// });
-
--- js/index.js
+++ js/index.js
@@ -8,3 +8,24 @@ xhr.onload = () => {
     console.log(xhr.response)
 }
 xhr.send()
+
+
+
+
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click', function handleClick() {
+//   btn.textContent = 'Спасибо!';
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const newButton = document.getElementById('btn');
+
+    newButton.addEventListener('click', function() {
+        // Переключаем класс active при нажатии
+        newButton.classList.toggle('active'); 
+        btn.textContent = 'Спасибо!';
+
+       
+    });
+});
